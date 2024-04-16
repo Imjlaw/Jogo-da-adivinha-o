d490927e54351461e54797a5bb48a49899108bcb
Ilson <Jilson788@gmail.com>
2024-04-16 00:59:06 -0300

    primeiro commit

---
+++ intourpar.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+if n % 2 == 0:
+    print('Este é um número par!')
+else:
+    print('Este é um número impar')
---
+++ list-int-arit.py
@@ -0,0 +1,17 @@
+
+numeros = input('Digite uma lista de números inteiros separados por espaço: ')
+# relembrar -> split = substring
+numeros = numeros.split()
+
+numeros = [int(num) for num in numeros]
+# relembrar -> sum = soma tudo | len = conta quantidade de elementos na lista
+media = sum(numeros) / len(numeros)
+# relembrar (dificil esquecer) -> max = maior número
+maior_numero = max(numeros)
+
+numeros_pares = sum(1 for num in numeros if num % 2 == 0)
+
+print('Média aritmética:', media)
+print('Maior número:', maior_numero)
+print('Quantidade de números pares:', numeros_pares)
+
